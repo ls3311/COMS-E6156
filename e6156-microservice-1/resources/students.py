d41d642ef1746250a74c7340b1e196d36ad7ff19
Lin Shi <shilin@Lins-MBP-3.lan>
2023-09-20 12:58:58 -0400

    Update directory

--- e6156-microservice-1/resources/students.py
+++ e6156-microservice-1/resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/shilin/Downloads/COMS E6156/task 1/e6156-microservice-1/resources/old-students.json"
+        "resources/old-students.json"
 
     def __init__(self):
         self.students = None
